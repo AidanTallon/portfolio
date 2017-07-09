5491eee169f2bb3907c156e76a14966bbd422167
AidanTallon <aidantallon@hotmail.com>
2017-07-09 15:02:59 +0100

    add nav buttons to developer. should be refactored at some point

--- app/assets/javascripts/developer.js
+++ app/assets/javascripts/developer.js
@@ -1,4 +1,5 @@
-var panels = ["monzostreak", "pokkenmatchuprecorder", "portfolio"];
+var panels = ["monzo_streak", "pokken_matchup_recorder", "portfolio"];
+var panelsNiceText = ["Monzo Streak", "Pokken Matchup Recorder", "Portfolio"]; // TODO: Automate
 
 var current_panel = 0;
 
@@ -30,9 +31,17 @@ function hidePanel(index)
 function showPanel(index)
 {
   getPanelDiv(panels[index]).style.display = "block";
+  document.getElementById("current-proj-label").innerHTML = panelsNiceText[index];
 }
 
 function getPanelDiv(panelName)
 {
   return document.getElementById("dev-" + panelName);
+}
+
+function changeToPanel(panelName)
+{
+  hidePanel(current_panel);
+  current_panel = panels.indexOf(panelName);
+  showPanel(current_panel);
 }
